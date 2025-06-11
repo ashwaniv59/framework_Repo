89ec9031516a190e0d1bb222c0ea303de4686109
Ashwani <ashwaniv59@gmail.com>
2025-06-11 15:18:54 +0530

    code added

---
+++ SampleSelenium/src/test/java/pac2/ContactTest.java
@@ -0,0 +1,19 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createContactTest() {
+		
+		
+		
+		System.out.println("execute createContactTest");
+	}
+	@Test
+	public void modifyContactTest() {
+		System.out.println("execute modifyContactTest");
+	}
+	
+}
