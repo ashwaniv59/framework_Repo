8bd9f7a9c6355412283e6ce6eac5327c747eb2ed
Ashwani <ashwaniv59@gmail.com>
2025-06-11 15:02:56 +0530

    second push

--- SampleSelenium/src/test/java/pac1/OrgTest.java
+++ SampleSelenium/src/test/java/pac1/OrgTest.java
@@ -6,6 +6,18 @@ public class OrgTest {
 	
 	@Test
 	public void createOrgTest() {
+		
+		String BROWSER=System.getProperty("browser");
+		String URL=System.getProperty("url");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+		
 		System.out.println("execute createOrgTest");
 	}
 	@Test
